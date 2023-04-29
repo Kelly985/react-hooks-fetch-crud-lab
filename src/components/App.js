fe6b29db503d4f0543f4d658379e411dba032b64
+ <+>
2023-04-29 07:50:50 +0300

    CRUD lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3099/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleAddQuestion = (newQuestion) => {
+    fetch("http://localhost:3099/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((response) => response.json())
+      .then((data) => setQuestions([...questions, data]))
+      .catch((error) => console.error(error));
+  };
+
+  const handleDeleteQuestion = (id) => {
+    fetch(`http://localhost:3099/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => setQuestions(questions.filter((question) => question.id !== id)))
+      .catch((error) => console.error(error));
+  };
+
+  const handleUpdateQuestion = (id, updatedQuestion) => {
+    fetch(`http://localhost:3099/questions/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedQuestion),
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        setQuestions(
+          questions.map((question) =>
+            question.id === id ? { ...data, id } : question
+          )
+        )
+      )
+      .catch((error) => console.error(error));
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={handleAddQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateQuestion={handleUpdateQuestion}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      const response = await fetch("http://localhost:3099/questions");
+      const data = await response.json();
+      setQuestions(data);
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} />
+        ))}
+      </ul>
     </section>
   );
 }
